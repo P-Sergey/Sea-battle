82b7609397841529aca4eae2586c825e75bb2dfd
P-Sergey <ravenheart.sp@gmail.com>
2020-06-18 20:19:53 +0300

    add vertical ships placing, code refactored

--- sea_battle.js
+++ sea_battle.js
@@ -1,21 +1,24 @@
-const letters = ['А','Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К'];
-let playerField = document.getElementById('player');
-let opponentField = document.getElementById('opponent');
+/* eslint-disable brace-style */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-plusplus */
+const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К'];
+const playerField = document.getElementById('player');
+const opponentField = document.getElementById('opponent');
 
-//making tables
+// making tables
 for (let i = 0; i < 11; i++) {
-  let playerRow = document.createElement('tr');
-  let opponentRow = document.createElement('tr');
-  
-  for (let k = 0; k < 11; k++) {
-    let playerCell = document.createElement('td');
-    let opponentCell = document.createElement('td');
+  const playerRow = document.createElement('tr');
+  const opponentRow = document.createElement('tr');
 
-    //remove unnecessary borders
+  for (let k = 0; k < 11; k++) {
+    const playerCell = document.createElement('td');
+    const opponentCell = document.createElement('td');
+
+    // remove unnecessary borders
     if (i === 0) {
       playerCell.classList.add('firstRow');
       opponentCell.classList.add('firstRow');
-    }   
+    }
 
     playerCell.classList.add('cell');
     playerRow.appendChild(playerCell);
@@ -23,29 +26,30 @@ for (let i = 0; i < 11; i++) {
     opponentRow.appendChild(opponentCell);
   }
 
-  //remove unnecessary borders
+  // remove unnecessary borders
   playerRow.cells[0].classList.add('firstColumn');
   opponentRow.cells[0].classList.add('firstColumn');
-  
+
   playerField.appendChild(playerRow);
   opponentField.appendChild(opponentRow);
 }
- 
 
-//set coordinates
-opponentField.rows[0].cells[0].setAttribute('id', 'coords');
+// set coordinates
+playerField.rows[0].cells[0].setAttribute('id', 'coords');
 opponentField.rows[0].cells[0].setAttribute('id', 'coords');
 
 for (let j = 1; j < playerField.rows.length; j++) {
-  playerField.rows[0].cells[j].innerHTML = letters[j-1];
+  playerField.rows[0].cells[j].innerHTML = letters[j - 1];
   playerField.rows[j].cells[0].innerHTML = j;
-  opponentField.rows[0].cells[j].innerHTML = letters[j-1];
-  opponentField.rows[0].cells[j].setAttribute('id', 'coords');
+  opponentField.rows[0].cells[j].innerHTML = letters[j - 1];
   opponentField.rows[j].cells[0].innerHTML = j;
+  playerField.rows[0].cells[j].setAttribute('id', 'coords');
+  playerField.rows[j].cells[0].setAttribute('id', 'coords');
+  opponentField.rows[0].cells[j].setAttribute('id', 'coords');
   opponentField.rows[j].cells[0].setAttribute('id', 'coords');
 }
 
-//shooting the opponent field
+// shooting the opponent field
 let targetCell;
 
 function highlight(node) {
@@ -53,65 +57,71 @@ function highlight(node) {
   targetCell.classList.add('highlight');
 }
 
-opponentField.onclick = function(event) {
-  let target = event.target;
-  while (target != this) {
-    if (target.tagName == 'TD' && target.id !== 'coords') {
+opponentField.onclick = function clickForShoot(event) {
+  let { target } = event;
+  while (target !== this) {
+    if (target.tagName === 'TD' && target.id !== 'coords') {
       highlight(target);
       return;
     }
     target = target.parentNode;
   }
-}
+};
 
-let coordList = document.createElement('div');
+const coordList = document.createElement('div');
 document.body.append(coordList);
 
-let message = document.createElement('span');
+const message = document.createElement('span');
 message.classList.add('message');
 message.innerHTML = 'Place 4-decks ship';
 playerField.after(message);
 
+playerField.onclick = function clickForShip(event) {
+  const { target } = event;
+  const targetX = target.cellIndex;
+  const targetY = target.parentNode.rowIndex;
+  const fourDecksShips = document.getElementsByClassName('fourDecks');
+  const threeDecksShips = document.getElementsByClassName('threeDecks');
+  const twoDecksShips = document.getElementsByClassName('twoDecks');
+  const oneDeckShips = document.getElementsByClassName('oneDeck');
 
-//placing ships
-playerField.onclick = function(event) {
-
-  let target = event.target;
-  let targetX = target.cellIndex;
-  let targetY = target.parentNode.rowIndex;
-  let fourDecksShips = document.getElementsByClassName('fourDecks');
-  let threeDecksShips = document.getElementsByClassName('threeDecks');
-  let twoDecksShips = document.getElementsByClassName('twoDecks');
-  let oneDeckShips = document.getElementsByClassName('oneDeck');
-  
+  // placing ships in horisontal direction
   function placeShipHorisontal(decks, shipClass) {
-    let afterShip = playerField.rows[targetY].cells[targetX + decks-1];
-    let beforeShip = playerField.rows[targetY].cells[targetX - 1];
-    
-    if ((targetX !== 0) && (targetY !== 0) && ((targetX + decks <= playerField.rows.length) &&
-    !target.classList.contains('ship') && !target.classList.contains('nearShip')) &&
-    !afterShip.classList.contains('nearShip') && !afterShip.classList.contains('ship')) {
+    const afterShip = playerField.rows[targetY].cells[targetX + decks - 1];
+    const beforeShip = playerField.rows[targetY].cells[targetX - 1];
 
-      //create ship
+    if (
+      targetX !== 0 &&
+      targetY !== 0 &&
+      targetX + decks <= playerField.rows.length &&
+      !target.classList.contains('ship') &&
+      !target.classList.contains('nearShip') &&
+      !afterShip.classList.contains('nearShip') &&
+      !afterShip.classList.contains('ship')
+    ) {
+      // create ship
       for (let i = 0; i < decks; i++) {
         playerField.rows[targetY].cells[targetX + i].classList.add('ship', shipClass);
       }
 
-      //create field around ship
+      // create field around ship
       for (let k = 0; k <= decks + 1; k++) {
-        let aboveShip = playerField.rows[targetY - 1].cells[(targetX - 1) + k];
-        let belowShip = playerField.rows[targetY + 1];
+        const aboveShip = playerField.rows[targetY - 1].cells[targetX - 1 + k];
+        const belowShip = playerField.rows[targetY + 1];
 
-        if (aboveShip !== undefined && !aboveShip.classList.contains('firstRow') &&
-          !aboveShip.classList.contains('firstColumn')) {
-            aboveShip.classList.add('nearShip');
+        if (
+          aboveShip !== undefined &&
+          !aboveShip.classList.contains('firstRow') &&
+          !aboveShip.classList.contains('firstColumn')
+        ) {
+          aboveShip.classList.add('nearShip');
         }
-        if (belowShip !== undefined && belowShip.cells[(targetX - 1) + k] !== undefined) {
-          belowShip.cells[(targetX - 1) + k].classList.add('nearShip');
+        if (belowShip !== undefined && belowShip.cells[targetX - 1 + k] !== undefined) {
+          belowShip.cells[targetX - 1 + k].classList.add('nearShip');
         }
         if (!beforeShip.classList.contains('firstColumn')) {
-             beforeShip.classList.add('nearShip');
-        }            
+          beforeShip.classList.add('nearShip');
+        }
         if (playerField.rows[targetY].cells[targetX + decks] !== undefined) {
           playerField.rows[targetY].cells[targetX + decks].classList.add('nearShip');
         }
@@ -119,15 +129,75 @@ playerField.onclick = function(event) {
     }
   }
 
-  //placing 4-decks ship
+  // placing ships in vertical direction
+  function placeShipVertical(decks, shipClass) {
+    const afterShip = playerField.rows[targetY + decks - 1];
+    const beforeShip = playerField.rows[targetY - 1].cells[targetX];
+
+    if (
+      targetX !== 0 &&
+      targetY !== 0 &&
+      targetY + decks - 1 !== undefined &&
+      !target.classList.contains('ship') &&
+      !target.classList.contains('nearShip') &&
+      afterShip !== undefined &&
+      afterShip.cells[targetX] !== undefined &&
+      !afterShip.cells[targetX].classList.contains('nearShip') &&
+      !afterShip.cells[targetX].classList.contains('ship') &&
+      !beforeShip.classList.contains('ship')
+    ) {
+      // create ship
+      for (let i = 0; i < decks; i++) {
+        playerField.rows[targetY + i].cells[targetX].classList.add('ship', shipClass);
+      }
+
+      // create field around ship
+      for (let k = 0; k <= decks + 1; k++) {
+        const rightOfShip = playerField.rows[targetY - 1 + k];
+        const leftOfShip = playerField.rows[targetY - 1 + k];
+
+        if (
+          leftOfShip !== undefined &&
+          leftOfShip.cells[targetX - 1] !== undefined &&
+          !leftOfShip.cells[targetX - 1].classList.contains('firstRow') &&
+          !leftOfShip.cells[targetX - 1].classList.contains('firstColumn')
+        ) {
+          leftOfShip.cells[targetX - 1].classList.add('nearShip');
+        }
+        if (
+          rightOfShip !== undefined &&
+          rightOfShip.cells[targetX + 1] !== undefined &&
+          !rightOfShip.cells[targetX + 1].classList.contains('ship') &&
+          !rightOfShip.cells[targetX + 1].classList.contains('firstRow')
+        ) {
+          rightOfShip.cells[targetX + 1].classList.add('nearShip');
+        }
+        if (
+          !beforeShip.classList.contains('firstColumn') &&
+          !beforeShip.classList.contains('firstRow')
+        ) {
+          beforeShip.classList.add('nearShip');
+        }
+        if (
+          playerField.rows[targetY + decks] !== undefined &&
+          !playerField.rows[targetY + decks].cells[targetX].classList.contains('ship')
+        ) {
+          playerField.rows[targetY + decks].cells[targetX].classList.add('nearShip');
+        }
+      }
+    }
+  }
+
+  // horisontal
+  // placing 4-decks ship
   if (fourDecksShips.length < 4) {
     placeShipHorisontal(4, 'fourDecks');
     if (fourDecksShips.length === 4) {
       message.innerHTML = 'Place 3-decks ship';
-    }    
+    }
   }
 
-  //placing 3-decks ships
+  // placing 3-decks ships
   else if (threeDecksShips.length < 6) {
     placeShipHorisontal(3, 'threeDecks');
     if (threeDecksShips.length === 6) {
@@ -135,7 +205,7 @@ playerField.onclick = function(event) {
     }
   }
 
-  //placing 2-decks ships
+  // placing 2-decks ships
   else if (twoDecksShips.length < 6) {
     placeShipHorisontal(2, 'twoDecks');
     if (twoDecksShips.length === 6) {
@@ -143,12 +213,45 @@ playerField.onclick = function(event) {
     }
   }
 
-  //placing 1-deck ships
+  // placing 1-deck ships
   else if (oneDeckShips.length < 4) {
     placeShipHorisontal(1, 'oneDeck');
     if (oneDeckShips.length === 4) {
       message.innerHTML = "All player's ships placed";
     }
   }
-  coordList.innerHTML = targetX + ' : ' + targetY ;
-}
+
+  // vertical
+  // placing 4-decks ship
+  if (fourDecksShips.length < 4) {
+    placeShipVertical(4, 'fourDecks');
+    if (fourDecksShips.length === 4) {
+      message.innerHTML = 'Place 3-decks ship';
+    }
+  }
+
+  // placing 3-decks ships
+  else if (threeDecksShips.length < 6) {
+    placeShipVertical(3, 'threeDecks');
+    if (threeDecksShips.length === 6) {
+      message.innerHTML = 'Place 2-decks ship';
+    }
+  }
+
+  // placing 2-decks ships
+  else if (twoDecksShips.length < 6) {
+    placeShipVertical(2, 'twoDecks');
+    if (twoDecksShips.length === 6) {
+      message.innerHTML = 'Place 1-deck ship';
+    }
+  }
+
+  // placing 1-deck ships
+  else if (oneDeckShips.length < 4) {
+    placeShipVertical(1, 'oneDeck');
+    if (oneDeckShips.length === 4) {
+      message.innerHTML = "All player's ships placed";
+    }
+  }
+  coordList.innerHTML = `${targetX} : ${targetY}`;
+};
